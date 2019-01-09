5962625cd446e02c8e1d4edda13d45fbfd829a32
Sam <sambartos@gmail.com>
2019-01-09 15:07:42 +1300

    initial testing setup

---
+++ game.js
@@ -0,0 +1,6 @@
+
+function printOut(frame) {
+    console.log(frame);
+}
+
+module.exports = printOut;
---
+++ test/printOut.test.js
@@ -0,0 +1,10 @@
+const printOut = require("../game")
+
+test("Checks if printout works", function () {
+    //Arrange
+    console.log("hello?")
+
+    //Act
+
+    //Assert
+})
